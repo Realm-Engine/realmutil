002f86877cf5d71afce7da3bed6b05606d3ab96d
Nathan Lapp <32725020+MashedTatos@users.noreply.github.com>
2022-09-26 23:11:24 -0400

    Work on vector

--- source/rmath.d
+++ source/rmath.d
@@ -1,199 +1,370 @@
 import std.stdio;
-version(D_SIMD)
+
+version (D_SIMD)
 {
-  import core.simd;
-  import std.traits;
-  import std.meta;
-  import std.range;
-  import std.algorithm;
-  import std.format;
-  pragma(msg,"SIMD Supported");
-  struct Vector(T, int N)
-  {
+    import core.simd;
+    import std.traits;
+    import std.meta;
+    import std.range;
+    import std.algorithm;
+    import std.format;
+    import std.math.operations;
+    import std.math.algebraic;
+    pragma(msg, "SIMD Supported");
 
-    public enum NumComponents = N;
-            
-              
-              
-    enum Components
+    enum bool IsSupportedVectorType(T, int n) = (is(T[n] == void[16])
+                || is(T[n] == byte[16]) || is(T[n] == ubyte[16])
+                || is(T[n] == short[8]) || is(T[n] == ushort[8])
+                || is(T[n] == int[4]) || is(T[n] == uint[4])
+                || is(T[n] == long[2]) || is(T[n] == ulong[2])
+                || is(T[n] == float[4]) || is(T[n] == double[2])
+                || is(T[n] == void[32]) || is(T[n] == byte[32])
+                || is(T[n] == ubyte[32]) || is(T[n] == short[16])
+                || is(T[n] == ushort[16]) || is(T[n] == int[8])
+                || is(T[n] == uint[8]) || is(T[n] == long[4])
+                || is(T[n] == ulong[4]) || is(T[n] == float[8]) || is(T[n] == double[8]));
+
+    enum NextMultiple(int N, int Multiple) = N + (Multiple - N % Multiple);
+    enum ComponentNames
+	{
+		x = 0,
+		y = 1,
+		z = 2,
+		w = 3
+	}
+    struct Vector(T, int N)
     {
-      x = 0,
-      y = 1,
-      z = 2,
-      w = 3
-    }
+        public enum NumComponents = N;
 
-
-    static assert(!isAggregateType!(T),"Vector type must be of floating or integral type");
-    
-    alias VectorType = Alias!(__vector(T[N])); 
-    private VectorType vector;
-
-    this(T val)
-    {
-      vector = val;
-    }
-
-    
-
-    this(VectorType other)
-    {
-      vector = other;
-    }
-
-    this(Range)(Range values) 
-    in(values.length <= N)
-    {
-      vector = 0;
-      for(int i = 0; i < values.length; i++)
-      {
-        vector.array[i] = values[i];
-
-      }
-
-    }
-
-    @property T[N] data() const
-    {
-      return vector.array;
-    }
-
-    auto opBinary(string op)(T v) const 
-    {
-       mixin("return typeof(this)(vector" ~ op ~ "v);");
-    }
-
-    auto opBinary(string op)(typeof(this) v) const
-    {
-      mixin("return typeof(this)(vector" ~ op ~ "v.vector);");
-
-    }
-
-  
-
-    bool opEquals( typeof(this)other) const
-    {
-      bool result = true;
-      auto aData = data;
-      auto bData = other.data;
-      for(int i = 0; i < NumComponents;i++)
-      {
-        if(aData[i] != bData[i])
+        static if (!IsSupportedVectorType!(T, N))
         {
-          result = false;
-        }
-      }
-      return result;
-    }
+            enum VectorSize = NextMultiple!(T.sizeof * N, 16) / T.sizeof;
 
-    private Components getComponentIndex(string Op)()
-    {
-      static foreach(Component; EnumMembers!(Components))
-      {
-        static if(Op == __traits(identifier,EnumMembers!(Components)[Component]))
-        {
-          return Component;
+            alias VectorType = Alias!(__vector(T[VectorSize]));
         }
+        else
+        {
+            enum VectorSize = N;
+            alias VectorType = Alias!(__vector(T[VectorSize]));
+        }
+
         
-      }
-      
-    }
 
-    @property auto opDispatch(const string Swizzle)() const if(Swizzle.length <= NumComponents && Swizzle.length > 1)
-   {
-     import std.conv;
-      enum SwizzleSize = Swizzle.length;
-      Vector!(T,NumComponents) result;
-      T[SwizzleSize] arr;
-      static foreach(SwizzleComponent; Swizzle)
-      {
+        static assert(!isAggregateType!(T), "Vector type must be of floating or integral type");
+        private VectorType vector;
+
+        this(T val)
         {
-          {
+            vector = val;
+        }
+
+        this(VectorType other)
+        {
+            vector = other;
+        }
+
+        private void makeVector(int i,Type, Tail...)(Type head, Tail tail)
+        in(i < NumComponents)
+		{
             
-            static foreach(Component;EnumMembers!(Components))
+		    static if(is(Type == T))
+			{
+                
+			    vector.array[i] = head;
+				static if(i + 1 < NumComponents)
+				{
+					makeVector!(i+1)(tail);
+				}
+			}
+            else static if(isStaticArray!(Type))
+			{
+                
+			    vector.array[i..i+Type.length] = head;
+                static if(i + Type.length < NumComponents)
+				{
+				    makeVector(i + Type.length,tail);
+				}
+                
+			}
+            
+            else
+			{
+			    static assert(false,"Cant construct vector with type " ~ Type.stringof);
+			}
+
+		       
+		}
+
+        
+
+        this(Args...)(Args args)
+		{
+		    makeVector!(0)(args);
+		
+		}
+
+        @property T[NumComponents] data() const
+        {
+            return vector.array[0 .. NumComponents];
+        }
+
+        
+
+        auto opBinary(string op)(T v) const
+        {
+            mixin("return typeof(this)(vector" ~ op ~ "v);");
+        }
+
+        auto opBinary(string op)(typeof(this) v) const if(op != "*")
+        {
+            mixin("return typeof(this)(vector" ~ op ~ "v.vector);");
+
+        }
+
+        float opBinary(string op)(typeof(this)other) const if(op == "*")
+		{
+            auto mul = this.vector * other.vector;
+            auto arr = mul.array;
+            float result = 0.0f;
+            for(int i = 0; i < NumComponents;i++)
+			{
+			    result += arr[i];
+			}
+            return result;
+
+		}
+
+        bool opEquals(typeof(this) other) const
+        {
+            bool result = true;
+            auto aData = data;
+            auto bData = other.data;
+            for (int i = 0; i < NumComponents; i++)
             {
-              
-              static if(SwizzleComponent.to!(string) == __traits(identifier,EnumMembers!(Components)[Component]))
-              {
-                arr[Component] = data[Component];
-              }
-
+                if (aData[i] != bData[i])
+                {
+                    result = false;
+                }
             }
-          }
-          
+            return result;
         }
-      }
-      result = Vector!(T,NumComponents)(arr);
-      return result;
 
-    }
-
-    @property T opDispatch(const string Op)() const if(Op.length == 1)
-    {
-      scope(failure)
-      {
-        writeln("Unable to get component " ~ Op);
-
-      }
-      static foreach(Component; EnumMembers!(Components))
-      {
-        
-        static if(Op == __traits(identifier,EnumMembers!(Components)[Component]))
+        @property auto opDispatch(const string Swizzle)() const 
+                if (Swizzle.length <= NumComponents && Swizzle.length > 1)
         {
-           
-          return data[Component];
+            import std.conv;
+
+            enum SwizzleSize = Swizzle.length;
+            Vector!(T, SwizzleSize) result;
+            T[SwizzleSize] arr;
+            int swizzleIndex = 0;
+            static foreach (SwizzleComponent; Swizzle)
+            {
+
+                static foreach (Component; EnumMembers!(ComponentNames))
+                {
+
+                    static if (SwizzleComponent.to!(string) == __traits(identifier,
+                            EnumMembers!(ComponentNames)[Component]))
+                    {
+                        arr[swizzleIndex] = data[Component];
+                    }
+
+                }
+                swizzleIndex++;
+            }
+            result = Vector!(T, SwizzleSize)(arr);
+            return result;
 
         }
-      }
-      
+
+        @property T opDispatch(const string Op)() const if (Op.length == 1)
+        {
+            static foreach (Component; EnumMembers!(ComponentNames))
+            {
+
+                static if (Op == __traits(identifier, EnumMembers!(ComponentNames)[Component]))
+                {
+
+                    return data[Component];
+
+                }
+            }
+
+        }
+
+        @property void opDispatch(const string Op)(T val) if(Op.length == 1)
+		{
+		    static foreach (Component; EnumMembers!(ComponentNames))
+            {
+
+                static if (Op == __traits(identifier, EnumMembers!(ComponentNames)[Component]))
+                {
+
+                    vector.ptr[Component] = val;
+
+                }
+            }
+		}
+
+        public float magnitude()
+		{
+            auto squared = vector * vector;
+            auto v = squared.array;
+            float result = 0;
+            for(int i = 0; i < NumComponents;i++)
+			{
+			    result += v[i];
+			}
+            return sqrt(result);
+
+
+		}
+
+        
+
     }
 
-  }
-  public alias vec4i = Alias!(Vector!(int,4));
-  public alias vec4 = Alias!(Vector!(float,4));
-  unittest
-  {
-      
-    vec4i v = vec4i(1);
-    assert(!__traits(compiles,v == Vector!(int,3)(1)));
-  }
-
-  unittest
-  {
-    vec4i v = vec4i(1);
-    vec4i v2 = vec4i(1);
-    assert(v == v2);
-           
-  }
-  unittest
-  {
-    vec4i v = vec4i(1) + vec4i(1);
-    assert(v == vec4i(2));
-    vec4 vf = vec4(1.0) + 1.0;
-    assert(vf == vec4(2.0));
-    assert(vf * 2.0 == vec4(4.0));
-    assert(vf * vec4(2.0) == vec4(4.0));
-
-  }
-
-  unittest
-  {
-    vec4 v = vec4([0.0,1.0,2.0,3.0]);
     
-    assert(v.x == 0.0);
-    assert(v.y == 1.0);
-    assert(v.z == 2.0);
-    assert(v.w == 3.0);
-    assert(v.xyz == Vector!(float,4)([0.0,1.0,2.0,0.0]));
-  
-  }
-     
+    public alias vec4 = Alias!(Vector!(float, 4));
+    public alias vec3 = Alias!(Vector!(float,3));
+    unittest
+    {
+        static assert(NextMultiple!(12, 16) == 16);
+        static assert(NextMultiple!(17, 16) == 32);
+    }
+
+    unittest
+	{
+	    
+        assert(vec4(1.0f,1.0f,1.0f,1.0f) == vec4(1.0f));
+        float[4] arr = [1.0f,1.0f,1.0f,1.0f];
+        assert(vec4(arr) == vec4(1.0f));
+        assert(vec4([1.0,1.0,1.0,1.0]) == vec4(1.0));
+        assert(vec4([1.0f,2.0f,3.0f,4.0f]) == vec4(1.0f,2.0f,3.0f,4.0f));
+        
+	}
+
+    unittest
+    {
+        vec4 v = vec4(1.0);
+        vec4 v2 = vec4(1.0);
+        assert(v == v2);
+
+    }
+
+    unittest
+    {
+        vec4 v = vec4(1.0) + vec4(1.0);
+        assert(v == vec4(2.0));
+        vec4 vf = vec4(1.0) + 1.0;
+        assert(vf == vec4(2.0));
+        assert(vf * 2.0 == vec4(4.0));
+        
+
+    }
+
+    unittest
+    {
+        vec4 v = vec4([0.0, 1.0, 2.0, 3.0]);
+
+        assert(v.x == 0.0);
+        assert(v.y == 1.0);
+        assert(v.z == 2.0);
+        assert(v.w == 3.0);
+        assert(v.xyz == vec3([0.0, 1.0, 2.0]));
+        assert(v.yxz == vec3([1.0,0.0,2.0]));
+
+    }
+
+    unittest
+	{
+	    vec3 v1 = vec3(1.0f);
+        const float expected = 1.73205;
+        float actual = v1.magnitude;
+        assert(isClose(actual,expected),"Expected ~ %f, got %f".format(expected,actual));
+	}
+    unittest
+	{
+	    vec3 v1 = vec3(1.0f,2.0f,3.0f);
+        vec3 v2 = vec3(4.0f,5.0f,6.0f);
+        const float expected = 32.0f;
+        float actual = v1 * v2;
+        assert(isClose(actual,expected),"Expected ~ %f, got %f".format(expected,actual));
+	}
+
+    unittest
+	{
+	    vec3 v = vec3(1.0f);
+        v.x = 2.0f;
+        assert(v.x == 2.0f);
+	}
+
+    struct Matrix(T,int Rows, int Columns)
+	{
+        alias VectorType = Vector!(T,Columns);
+        private Vector!(T,Columns)[Rows] matrix;
+        
+        private void makeMatrix(int x,int y, Type, Tail...)(Type head, Tail tail)
+		{
+            static if(is(Type == Vector!(T,Columns)))
+			{
+			    matrix[y] = head;
+                makeMatrix(0,y + 1)(tail);
+			}
+            else static if(isStaticArray!(Type))
+			{
+			    
+			}
+
+		}
+
+
+        this(Args...)(Args args)
+		{
+		    makeMatrix!(0,0)(args);
+		}
+        this(T val)
+		{
+		    for(int i = 0; i < Rows;i++)
+			{
+			    matrix[i] = VectorType(val);
+			}
+		
+		}
+
+        @property auto ref opDispatch(const string Op)() if(Op.length == 1)
+		{
+		    static foreach (Component; EnumMembers!(ComponentNames))
+            {
+
+                static if (Op == __traits(identifier, EnumMembers!(ComponentNames)[Component]))
+                {
+
+                    return matrix[Component];
+
+                }
+            }
+		}
+
+        
+
+	}
+    alias mat4 = Alias!(Matrix!(float,4,4));
+    unittest
+	{
+	    mat4 mat = mat4(vec4(1.0),vec4(1.0),vec4(1.0),vec4(1.0));
+        assert(mat.x == vec4(1.0f));
+	    mat = mat4(1.0f);
+        assert(mat.x == vec4(1.0f));
+        mat.x.x = 2.0f;
+        assert(mat.x.x == 2.0f);
+
+	}
+
 }
 else
 {
-  pragma(msg,"SIMD not supported!");
-
+    pragma(msg, "SIMD not supported!");
 
 }
-
-
